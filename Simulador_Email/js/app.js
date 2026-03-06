b5da216158a78b1619d786804d4d1e45079c78b6
Arcus <stiven55sol@gmail.com>
2026-03-05 23:52:01 -0500

    feat: Validacion de campos del formulario

---
+++ Simulador_Email/js/app.js
@@ -0,0 +1,45 @@
+const $Email = document.querySelector('#email');
+const $Asunto = document.querySelector('#asunto');
+const $Mensaje = document.querySelector('#mensaje');
+
+// Asignando eventos
+
+$Email.addEventListener('blur', validar);
+$Asunto.addEventListener('blur', validar);
+$Mensaje.addEventListener('blur', validar);
+
+//Validando campos
+
+function validar(e){
+    if(e.target.value.trim() === ''){
+        MostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+    }
+}
+
+//Creando Alertas
+
+function MostrarAlerta(mensaje, referencia){
+    //Limpiando alertas
+    limpiarAlertas(referencia);
+
+    //Creando Alerta HTML
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+
+    //Agregando alerta
+    referencia.appendChild(error);
+}
+
+
+function limpiarAlertas(referencia){
+    
+    //Direccion de la alerta
+    const alerta = referencia.querySelector('.error');
+    
+    //Validadndo si existe
+    if(alerta){
+        alerta.remove(); //Removiendo alerta
+    }
+
+}
